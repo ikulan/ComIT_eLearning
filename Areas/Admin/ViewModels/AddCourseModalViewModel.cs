459486e6e32866f8eb3f7759139320757f25cb1f
ikulan <ikulan12@gmail.com>
2025-04-18 15:23:56 -0700

    Rename modal files

--- Areas/Admin/ViewModels/AddCourseModalViewModel.cs
+++ Areas/Admin/ViewModels/AddCourseModalViewModel.cs
@@ -1,6 +1,6 @@
 namespace ComIT_eLearning.Models
 {
-  public class CoursesModalViewModel
+  public class AddCourseModalViewModel
   {
     public string StudentId { get; set; }
     public string AddCourseUrl { get; set; }
